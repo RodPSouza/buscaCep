7935dda587b6ebb0df56f56b3e0de4ad42fd8dac
ADM <rod.p.souza@hotmail.com>
2025-04-02 14:17:31 -0300

    Processo Nava / Projeto Santander - Desafio Técnico

---
+++ src/main/java/br/com/rsouza8891/buscaCep/BuscaCepApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsouza8891.buscaCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsouza8891/buscaCep/BuscaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsouza8891.buscaCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
